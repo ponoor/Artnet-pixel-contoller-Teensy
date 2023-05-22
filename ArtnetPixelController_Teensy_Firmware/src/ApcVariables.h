1439c8fd9ec6721ae23cf38ae29f570b1ff22936
niimi <niimitaiki@gmail.com>
2023-05-22 11:05:12 +0900

    variables

--- ArtnetPixelController_Teensy_Firmware/src/ApcVariables.h
+++ ArtnetPixelController_Teensy_Firmware/src/ApcVariables.h
@@ -7,9 +7,9 @@ namespace apc {
     namespace variables {
         namespace defaultSettings {
             namespace network {
-                static const IPAddress ip(10, 0, 1, 0);
-                static const IPAddress gateway(10, 0, 0, 1);
-                static const IPAddress subnet(255, 255, 0, 0);
+                static const IPAddress ip(10, 0, 0, 0);
+                static const IPAddress gateway(10, 0, 0, 200);
+                static const IPAddress subnet(255, 255, 255, 0);
                 static const uint8_t mac[6] = {0x60, 0x95, 0xCE, 0x10, 0x07, 0x00};//60:95:CE:10:07:xx 
                 static const uint8_t ipAddId = 1;
                 static const uint8_t macAddId = 1;
@@ -25,14 +25,14 @@ namespace apc {
             }
             namespace osc {
                 static const int dstPort = 50000;
-                static const IPAddress dstIp(10, 0, 0, 10);
+                static const IPAddress dstIp(10, 0, 0, 201);
                 static const int srcPort = 50000;
                 static const uint8_t dstPortAddId = 0;
             }
             namespace led {
                 static const int numPixels = 680;
                 static const uint8_t protocol = WS2811_800kHz;
-                static const uint8_t colorOrder = WS2811_GRB;
+                static const uint8_t colorOrder = WS2811_RGB;
                 static const uint8_t initTest = 1;
             }
         }
